8550be6dfc9946662d9a64373a45e7a19ef946da
jerem-mrt <39987382+jerem-mrt@users.noreply.github.com>
2018-06-06 21:13:31 +0200

    Nouvelles fonctions pour la connexion
    + verifyDefinedName : verifie que les name sont bien définis (donc que chaque champ est rempli
    + verifierChamps : verifie que chaque champ a été rempli
    - Adaptation de verifyEmail au passage à PDO

--- func_login.php
+++ func_login.php
@@ -6,14 +6,53 @@
  * and open the template in the editor.
  */
 include 'database.php';
+// Vérifier que chacun des champs est bien rempli.
+function verifyDefinedName ($listeName) {
+    $err = 1;
+    foreach ($listeName as $key){
+        if (isset($_POST[$key])) {
+            $err = 0;
+        }
+    }
+    if ($err === 0) {
+        return true;
+    }
+}
+function verifierChamps() {
+    //var_dump($_POST);
+    $err = [];
+foreach ($_POST as $key => $value) {
+    if (!empty($value)){
+        
+    }
+    
+    else {
+        $err[] = "Le champ " . $key . " n'a pas été complété. <br />";
+    }
+}
+if (sizeof($err) !==0) {
+    foreach ($err as $key) {
+        echo $key;
+    }
+    return false;
+}
+else {
+    return true;
+}
+//var_dump($err);
+}
+
 
 // On vérifie que l'utilisateur n'est pas déjà inscrit. Retourne TRUE s'il n'est pas déjà inscrit. 
-function verifyEmail ($email) {
-    $query = "SELECT * FROM nounou WHERE email='" . $email . "';";
-    $resultat = mysqli_query($GLOBALS['database'], $query);
-   
-    if (mysqli_num_rows($resultat) === 0) {
-    return true;
+function verifyEmail ($bd, $email) {
+    $requete = $bd->query ("SELECT * FROM nounou WHERE email='" . $email . "';");
+    
+   // On vérifie qu'aucune adresse e-mail correspondante n'est renvoyée.
+    if ($requete) {
+    return false;
+    }
+    else {
+        return true;
     }
 };
 
