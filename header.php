2b87c7af2e24aafeb0849542acf1f54c99bf4c70
AdrienLebret <39364576+AdrienLebret@users.noreply.github.com>
2018-06-08 17:17:20 +0200

    Un projet web par Adrien LEBRET et Jérémie MAROTTE
    Juste changer "Annonces de nounous près de chez nous" par "Un projet web par Adrien LEBRET et Jérémie MAROTTE"

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
                <div class="row">
 
                     <div class="col-md-4 col-sm-5">
-                         <p>Annonces de nounous près de chez vous</p>
+                         <p>Un Projet Web par Adrien LEBRET et Jérémie MAROTTE</p>
                     </div>
                          
                     <div class="col-md-8 col-sm-7 text-align-right">
