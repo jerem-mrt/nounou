eed490c54f4a3f612e0420b61527965a636de811
jerem-mrt <39987382+jerem-mrt@users.noreply.github.com>
2018-06-11 16:44:08 +0200

    Factorisation de menu.php et inclusion de menu.php

--- index.php
+++ index.php
@@ -38,36 +38,7 @@
 
     
 
-     <!-- MENU -->
-     <section class="navbar navbar-default navbar-static-top" role="navigation">
-          <div class="container">
-
-               <div class="navbar-header">
-                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                    </button>
-
-                    <!--Nom du site-->
-                    <a href="index.php" class="navbar-brand">Looking For Nounou</a>
-                    <!--<a href="index.php" class="navbar-brand"><img src=" health-center-master/images/l4n-image_mini.jpg" /></a>-->
-               </div>
-
-               <!-- MENU LINKS -->
-               <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                         <!-- <li><a href="#top" class="smoothScroll">Home</a></li>
-                         <li><a href="#about" class="smoothScroll">About Us</a></li>
-                         <li><a href="#team" class="smoothScroll">Doctors</a></li>
-                         <li><a href="#news" class="smoothScroll">News</a></li>
-                         <li><a href="#google-map" class="smoothScroll">Contact</a></li>-->
-                         <li class="appointment-btn"><a href="login.php">Se connecter</a></li>
-                    </ul>
-               </div>
-
-          </div>
-     </section>
+     <?php include 'menu.php'; ?>
 
 
      <!-- HOME -->
