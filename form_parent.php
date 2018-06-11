9cc4a0f77c61bcaa68d73cf442b9254032d029d5
AdrienLebret <39364576+AdrienLebret@users.noreply.github.com>
2018-06-11 22:26:03 +0200

    Form_Action MAJ
    Je vais y arriver !

--- form_parent.php
+++ form_parent.php
@@ -2,6 +2,7 @@
 include 'database.php';
 include 'form.php';
 include 'css.php';
+include 'header.php';
 ?><!DOCTYPE html>
 <html lang="en">
 
@@ -45,33 +46,7 @@ include 'css.php';
             </div>
         </section>
 
-
-        <!-- HEADER -->
-        <header>
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-md-4 col-sm-5">
-                        <p>Looking For Nounou</p>
-                    </div>
-
-                    <div class="col-md-8 col-sm-7 text-align-right">
-                        <span class="phone-icon"><i class="fa fa-phone"></i> 010-060-0160</span>
-                        <span class="date-icon"><i class="fa fa-calendar-plus-o"></i> 6:00 AM - 10:00 PM (Mon-Fri)</span>
-                        <span class="email-icon"><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></span>
-                    </div>
-
-                </div>
-            </div>
-        </header>
-
-
-
-
-
-
-
-        <!-- MAKE AN APPOINTMENT -->
+        <!-- PARTIE FORMULAIRE-->
         <section id="appointment" data-stellar-background-ratio="3">
             <div class="container">
                 <div class="row">
@@ -98,11 +73,6 @@ include 'css.php';
                                 <div class="col-md-12 col-sm-12 ui-widget">
                                     <label for="nomV">Ville</label><p></p>
                                     <input id="nomV" type="text" name="nomV" placeholder="Votre ville">
-                                </div>
-
-                                <div class="col-md-12 col-sm-12">
-                                    <label for="telephoneP">N° de téléphone</label>
-                                    <input type="tel" class="form-control" id="phone" name="telephoneP" placeholder="Téléphone">
                                 </div>
 
                                 <div class="col-md-12 col-sm-12">
@@ -133,7 +103,8 @@ include 'css.php';
                                     <textarea class="form-control" rows="5" id="remarques" name="remarques" placeholder="Si vos enfants ont des contraintes alimentaires, des allergies..."></textarea>
                                 </div>-->
 
-                                <button type="submit" class="form-control" id="cf-submit" name="submit">Envoyer</button>
+                                <button type="submit" class="form-control" id="cf-submit">Envoyer</button>
+                                
                             </div>
                     </div>
                     </form>
@@ -144,91 +115,7 @@ include 'css.php';
     </section>
 
 
-    <!-- FOOTER -->
-    <footer data-stellar-background-ratio="5">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-4 col-sm-4">
-                    <div class="footer-thumb">
-                        <h4 class="wow fadeInUp" data-wow-delay="0.4s">Contact Info</h4>
-                        <p>Fusce at libero iaculis, venenatis augue quis, pharetra lorem. Curabitur ut dolor eu elit consequat ultricies.</p>
-
-                        <div class="contact-info">
-                            <p><i class="fa fa-phone"></i> 010-070-0170</p>
-                            <p><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-sm-4">
-                    <div class="footer-thumb">
-                        <h4 class="wow fadeInUp" data-wow-delay="0.4s">Latest News</h4>
-                        <div class="latest-stories">
-                            <div class="stories-image">
-                                <a href="#"><img src="images/news-image.jpg" class="img-responsive" alt=""></a>
-                            </div>
-                            <div class="stories-info">
-                                <a href="#"><h5>Amazing Technology</h5></a>
-                                <span>March 08, 2018</span>
-                            </div>
-                        </div>
-
-                        <div class="latest-stories">
-                            <div class="stories-image">
-                                <a href="#"><img src="images/news-image.jpg" class="img-responsive" alt=""></a>
-                            </div>
-                            <div class="stories-info">
-                                <a href="#"><h5>New Healing Process</h5></a>
-                                <span>February 20, 2018</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-sm-4">
-                    <div class="footer-thumb">
-                        <div class="opening-hours">
-                            <h4 class="wow fadeInUp" data-wow-delay="0.4s">Opening Hours</h4>
-                            <p>Monday - Friday <span>06:00 AM - 10:00 PM</span></p>
-                            <p>Saturday <span>09:00 AM - 08:00 PM</span></p>
-                            <p>Sunday <span>Closed</span></p>
-                        </div>
-
-                        <ul class="social-icon">
-                            <li><a href="https://www.facebook.com/tooplate" class="fa fa-facebook-square" attr="facebook icon"></a></li>
-                            <li><a href="#" class="fa fa-twitter"></a></li>
-                            <li><a href="#" class="fa fa-instagram"></a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-md-12 col-sm-12 border-top">
-                    <div class="col-md-4 col-sm-6">
-                        <div class="copyright-text">
-                            <p>Copyright &copy; 2017 Your Company
-
-                                | Design: <a href="http://www.tooplate.com" target="_parent">Tooplate</a></p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-6">
-                        <div class="footer-link">
-                            <a href="#">Laboratory Tests</a>
-                            <a href="#">Departments</a>
-                            <a href="#">Insurance Policy</a>
-                            <a href="#">Careers</a>
-                        </div>
-                    </div>
-                    <div class="col-md-2 col-sm-2 text-align-center">
-                        <div class="angle-up-btn">
-                            <a href="#top" class="smoothScroll wow fadeInUp" data-wow-delay="1.2s"><i class="fa fa-angle-up"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </footer>
+    
     <!-- SCRIPTS -->
 <?php
 script("bootstrap.min.js");
@@ -244,7 +131,6 @@ script("custom.js");
             $("#nomV").autocomplete({
                 source: 'rechercheVille.php',
                 minLength: 2 // on indique qu'il faut taper au moins 3 caractères pour afficher l'autocomplétion
-
             });
         });
     </script>
